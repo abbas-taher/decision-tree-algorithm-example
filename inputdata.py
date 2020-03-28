9c292133f03de47207853c60f3c0fc707acadc62
Abbas Taher <abbtah@yahoo.com>
2020-03-28 06:19:10 -0400

    Update inputdata.py

--- inputdata.py
+++ inputdata.py
@@ -1,5 +1,5 @@
 def createDataset():
-    dataSet = [[1, 1, 'yes'],
+    dataset = [[1, 1, 'yes'],
                [1, 1, 'yes'],
                [1, 0, 'no'],
                [0, 1, 'no'],
@@ -10,4 +10,4 @@ def createDataset():
     features = ['non-surfacing','flippers']
     label = ['isfish']
     
-    return dataSet, features
+    return dataset, features
